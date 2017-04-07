dc95c7f5963479c468b6bbcb76d12ba6f9866313
Liz Rodriguez <ecrodriguez@gmail.com>
2017-04-07 09:52:08 -0400

    add json client.js

--- public/js/client.js
+++ public/js/client.js
@@ -1,30 +1,29 @@
 $(document).ready(function() {
 
+  let trainData = "";
   $('select').material_select();
-
 
   $('button').on('click',function(){
     let $departure = $('[name="departure"]').val();
     console.log("The 3 letter departure station is: " + $departure);
+
     let $arrival = $('[name="arrival"]').val();
     console.log("The 3 letter arrival station is: " + $arrival);
 
-      // $.ajax({
-      //   url:'http://localhost:3000/trainlist/',
-      //   method:'POST',
-      //   success: function(data){
-      //     console.log(data);
-      //       // $.ajax({
-      //       //   url: 'https://traintime.lirr.org/api/Departure?loc=' + data.shortname,
-      //       //   method: 'GET',
-      //       //   success: function(data){
-      //       //     console.log("the JSON object is:" + data)
-      //       //   }
-      //       // })
-      //   }
-      // })
 
-  })
+    $.ajax({ //https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+      url: 'https://traintime.lirr.org/api/Departure?loc=' + $departure,
+      method: 'GET',
+      dataType: 'json',
+      success: function(response){
+        console.log(response.LOC);
+        console.log(response.TIME);
+        
+        }
+    })
+
+
+  }) //button
 
 
 
